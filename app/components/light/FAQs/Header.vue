e19f9af12fab8d3b1c1df5cf1a3cf35668cd981d
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-05-11 16:47:04 +0700

    feat: add Header component for FAQs section with background image and title

---
+++ app/components/light/FAQs/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <header class="header page-header bg-img section-padding" data-background="/light/assets/imgs/header/bg1.jpg"
+    data-overlay-dark="9">
+    <div class="container pt-100 ontop">
+      <div class="text-center">
+        <h1 class="fz-100 text-u">FAQs.</h1>
+        <div class="mt-15">
+          <a href="/">Home</a>
+          <span class="padding-rl-20">|</span>
+          <span class="main-color">FAQs</span>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<script setup>
+import loadBackgroudImages from '@/common/loadBackgroudImage';
+
+onMounted(() => {
+  loadBackgroudImages();
+});
+</script>
