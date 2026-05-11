e10ac1813f9bbdb6a9cfd5bfc0f2b27881e1f6df
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-05-11 16:47:20 +0700

    feat: add Blog component with articles and author details

---
+++ app/components/light/home-main/Blog.vue
@@ -0,0 +1,101 @@
+<template>
+  <section class="blog-list-half section-padding sub-bg">
+    <div class="container">
+      <div class="sec-head mb-80">
+        <h6 class="sub-title main-color mb-25">Sharp Thinking on the AI Transition</h6>
+        <div class="bord pt-25 bord-thin-top d-flex align-items-center">
+          <h2 class="fw-600 d-rotate wow">
+            <span class="rotate-text">
+              What we're seeing <span class="fw-200">in the field.</span>
+            </span>
+          </h2>
+          <div class="ml-auto">
+            <a href="/insights" class="go-more">
+              <span class="text">Read All Insights</span>
+              <span class="icon ti-arrow-top-right"></span>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6">
+          <div class="item main-bg md-mb50">
+            <div class="row rest">
+              <div class="col-lg-6 col-md-5 img rest">
+                <img src="/light/assets/imgs/blog/b/2.jpg" alt="" class="img-post" />
+                <div class="author d-flex align-items-center">
+                  <div>
+                    <div class="fit-img icon-img-50 circle">
+                      <img src="/light/assets/imgs/blog/author.png" alt="" />
+                    </div>
+                  </div>
+                  <div class="ml-15 fz-14">
+                    <div>
+                      <span class="opacity-7">Posted by</span>
+                      <br />
+                      MAXY Digital
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-7 cont valign">
+                <div class="full-width">
+                  <div class="tags mb-15">
+                    <a href="/insights">Agentic AI</a>
+                  </div>
+                  <h5>
+                    <a href="/insights">
+                      Why agentic AI beats automation every time.
+                    </a>
+                  </h5>
+                  <span class="date fz-12 ls1 text-u opacity-7 mt-80">
+                    March 2025
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col-lg-6">
+          <div class="item main-bg">
+            <div class="row rest">
+              <div class="col-lg-6 col-md-5 img rest">
+                <img src="/light/assets/imgs/blog/b/3.jpg" alt="" class="img-post" />
+                <div class="author d-flex align-items-center">
+                  <div>
+                    <div class="fit-img icon-img-50 circle">
+                      <img src="/light/assets/imgs/blog/author.png" alt="" />
+                    </div>
+                  </div>
+                  <div class="ml-15 fz-14">
+                    <div>
+                      <span class="opacity-7">Posted by</span>
+                      <br />
+                      MAXY Digital
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-lg-6 col-md-7 cont valign">
+                <div class="full-width">
+                  <div class="tags mb-15">
+                    <a href="/insights">Strategy</a>
+                    <a href="/insights">Singapore</a>
+                  </div>
+                  <h5>
+                    <a href="/insights">
+                      How Singapore's PSG grant de-risks your first AI project.
+                    </a>
+                  </h5>
+                  <span class="date fz-12 ls1 text-u opacity-7 mt-80">
+                    February 2025
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
