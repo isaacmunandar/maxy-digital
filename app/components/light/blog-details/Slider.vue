6d5ade02951c424f7a1edb9e06e0782062469305
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-05-11 16:44:56 +0700

    feat: add Slider component for blog details page

---
+++ app/components/light/blog-details/Slider.vue
@@ -0,0 +1,60 @@
+<template>
+  <header class="blog-header section-padding pb-0">
+    <div class="container mt-80">
+      <div class="row justify-content-center">
+        <div class="col-lg-10">
+          <div class="caption">
+            <div class="sub-title fz-12">
+              <a href="/insights">
+                <span>Agentic AI , </span>
+              </a>
+              <a href="/insights">
+                <span>Strategy</span>
+              </a>
+            </div>
+            <h1 class="fz-55 mt-30">
+              Why agentic AI beats automation every time.
+            </h1>
+          </div>
+          <div class="info d-flex mt-40 align-items-center">
+            <div class="left-info">
+              <div class="d-flex align-items-center">
+                <div class="author-info">
+                  <div class="d-flex align-items-center">
+                    <a href="#0" class="circle-60">
+                      <img src="/light/assets/imgs/blog/author.png" alt="" class="circle-img" />
+                    </a>
+                    <a href="/insights" class="ml-20">
+                      <span class="opacity-7">Author</span>
+                      <h6 class="fz-16">MAXY Digital</h6>
+                    </a>
+                  </div>
+                </div>
+                <div class="date ml-50">
+                  <a href="/insights">
+                    <span class="opacity-7">Published</span>
+                    <h6 class="fz-16">March 2025</h6>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="right-info ml-auto">
+              <div>
+                <span class="pe-7s-comment fz-18 mr-10"></span>
+                <span class="opacity-7">02 Comments</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="background bg-img mt-80" data-background="/light/assets/imgs/blog/b1.jpg"></div>
+  </header>
+</template>
+<script setup>
+import loadBackgroudImages from '@/common/loadBackgroudImage';
+
+onMounted(() => {
+  loadBackgroudImages();
+});
+</script>
