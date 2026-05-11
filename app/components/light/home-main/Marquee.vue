81015d2101f8a331b48753608ea5c1b96ba68da2
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-05-11 16:48:04 +0700

    feat: add Marquee component with key messaging and promotional highlights

---
+++ app/components/light/home-main/Marquee.vue
@@ -0,0 +1,72 @@
+<template>
+  <section>
+    <div class="main-marq xlrg o-hidden">
+      <div class="slide-har st1">
+        <div class="box">
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>Up to 50% Co-Funded by Singapore Government</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>PSG and EDG Grants Available</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>AI Systems Deployed in SG · US · ID</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>HolonIQ Top 50 SEA EdTech 2025</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>Deploy in 6 Weeks. Not 6 Months.</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+        </div>
+        <div class="box">
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>Up to 50% Co-Funded by Singapore Government</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>PSG and EDG Grants Available</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>AI Systems Deployed in SG · US · ID</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>HolonIQ Top 50 SEA EdTech 2025</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+          <div class="item">
+            <h4 class="d-flex align-items-center">
+              <span>Deploy in 6 Weeks. Not 6 Months.</span>
+              <span class="fz-50 ml-50 stroke icon">*</span>
+            </h4>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
