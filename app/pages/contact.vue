0072146227f78c959896a24f7997314445406fc3
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-05-15 15:50:03 +0700

    fix: comment out LoadingScreen import and usage in contact.vue

--- app/pages/contact.vue
+++ app/pages/contact.vue
@@ -1,5 +1,5 @@
 <template>
-  <LoadingScreen />
+  <!-- <LoadingScreen /> -->
   <Cursor />
   <ProgressScroll />
   <Lines />
@@ -19,7 +19,7 @@ import Footer from '@/components/light/blog-details/Footer';
 import Lines from '@/components/light/common/Lines';
 import ProgressScroll from '@/components/light/common/ProgressScroll';
 import Cursor from '@/components/light/common/cusor';
-import LoadingScreen from '@/components/light/common/loader';
+// import LoadingScreen from '@/components/light/common/loader';
 import Contact from '@/components/light/contact/Contact';
 import Header from '@/components/light/contact/Header';
 import Navbar from '@/components/light/creative-agency/Navbar';
