7a179d13c2b3e8cab9e400222ca19683414a9f78
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-06-08 11:09:54 +0700

    fix: remove duplicate item for PSG/EDG Grant-Aligned in Marquee component

--- app/components/light/about/Marquee.vue
+++ app/components/light/about/Marquee.vue
@@ -23,12 +23,6 @@
           </div>
           <div class="item">
             <h4 class="d-flex align-items-center">
-              <span>PSG/EDG Grant-Aligned for Singapore SMEs</span>
-              <span class="fz-50 ml-50 stroke icon">*</span>
-            </h4>
-          </div>
-          <div class="item">
-            <h4 class="d-flex align-items-center">
               <span>12-Month Roadmap, Not a Slide Deck</span>
               <span class="fz-50 ml-50 stroke icon">*</span>
             </h4>
@@ -50,12 +44,6 @@
           <div class="item">
             <h4 class="d-flex align-items-center">
               <span>Phase 03: AI Transformation Infrastructure</span>
-              <span class="fz-50 ml-50 stroke icon">*</span>
-            </h4>
-          </div>
-          <div class="item">
-            <h4 class="d-flex align-items-center">
-              <span>PSG/EDG Grant-Aligned for Singapore SMEs</span>
               <span class="fz-50 ml-50 stroke icon">*</span>
             </h4>
           </div>
