cee53bfb68d3df48a339239d1a470e62fc023fd4
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-07-17 10:26:27 +0700

    feat(solution): reveal FAQ section

--- app/components/light/services-details/FAQs.vue
+++ app/components/light/services-details/FAQs.vue
@@ -2,23 +2,23 @@
   <section class="faqs section-padding pt-0">
     <div class="container">
       <div class="row justify-content-between">
-        <div class="col-lg-5 valign">
+        <div v-reveal class="col-lg-5 valign">
           <div class="faq-img md-mb50">
             <img src="/light/assets/imgs/intro/03.png" alt="" />
           </div>
         </div>
         <div class="col-lg-6 valign">
           <div class="full-width">
-            <div class="sec-head mb-50">
+            <div v-reveal class="sec-head mb-50">
               <h6 class="sub-title main-color mb-15">{{ faqContent.subTitle }}</h6>
               <h3>{{ faqContent.heading }}</h3>
             </div>
             <div class="list-serv">
-              <div class="accordion bord">
+              <div class="accordion bord" v-reveal:stagger="{ delay: 100 }">
                 <div
                   v-for="(item, index) in faqContent.items"
                   :key="item.q"
-                  class="item mb-15 wow fadeInUp"
+                  class="item mb-15"
                   :class="{ active: index === 0 }"
                 >
                   <div @click="openAccordion" class="title">
