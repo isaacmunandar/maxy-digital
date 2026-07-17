0d1cbd7df47200617d3d92061af9aaddf30eea3b
Ghufron Bagaskara <ghufronbagaskara08@gmail.com>
2026-07-17 10:26:29 +0700

    feat(solutions): polish engagement cards

--- app/components/light/services/Price.vue
+++ app/components/light/services/Price.vue
@@ -5,29 +5,24 @@
         <div class="col-lg-5 valign">
           <div class="cont full-width md-mb50">
             <div>
-              <h6 class="sub-title main-color mb-15 wow fadeIn">
+              <h6 v-reveal class="sub-title main-color mb-15">
                 How clients engage with us
               </h6>
-              <h3 class="d-slideup wow">
-                <span class="sideup-text">
-                  <span class="up-text">Three entry points.</span>
-                </span>
-                <span class="sideup-text">
-                  <span class="up-text">One outcome.</span>
-                </span>
+              <h3 v-reveal:clip="{ delay: 80 }">
+                Three entry points. One outcome.
               </h3>
               <div class="text mt-20">
-                <p>
+                <p v-reveal="{ delay: 160 }">
                   Indicative ranges for SMEs and enterprises. Final scope sized to your problem, not a price card.
                 </p>
               </div>
-              <ul class="rest list-arrow mt-30 pt-30 bord-thin-top">
+              <ul class="rest list-arrow mt-30 pt-30 bord-thin-top" v-reveal:stagger="{ delay: 220 }">
                 <li>
                   <span class="icon">
                     <svg width="100%" height="100%" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path fillRule="evenodd" clipRule="evenodd"
                         d="M7.71108 3.78684L8.22361 4.29813L7.71263 4.80992L4.64672 7.87832L4.13433 7.36688L6.87531 4.62335H1.11181H0.750039H0.388177L0.382812 0.718232H1.10645L1.11082 3.90005H6.80113L4.12591 1.22972L4.63689 0.718262L7.71108 3.78684Z"
-                        fill="#14cf93"></path>
+                        fill="#2563eb"></path>
                     </svg>
                   </span>
                   <h6 class="inline fw-400">Production code. Not a slide deck.</h6>
@@ -37,7 +32,7 @@
                     <svg width="100%" height="100%" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path fillRule="evenodd" clipRule="evenodd"
                         d="M7.71108 3.78684L8.22361 4.29813L7.71263 4.80992L4.64672 7.87832L4.13433 7.36688L6.87531 4.62335H1.11181H0.750039H0.388177L0.382812 0.718232H1.10645L1.11082 3.90005H6.80113L4.12591 1.22972L4.63689 0.718262L7.71108 3.78684Z"
-                        fill="#14cf93"></path>
+                        fill="#2563eb"></path>
                     </svg>
                   </span>
                   <h6 class="inline fw-400">
@@ -49,7 +44,7 @@
                     <svg width="100%" height="100%" viewBox="0 0 9 8" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path fillRule="evenodd" clipRule="evenodd"
                         d="M7.71108 3.78684L8.22361 4.29813L7.71263 4.80992L4.64672 7.87832L4.13433 7.36688L6.87531 4.62335H1.11181H0.750039H0.388177L0.382812 0.718232H1.10645L1.11082 3.90005H6.80113L4.12591 1.22972L4.63689 0.718262L7.71108 3.78684Z"
-                        fill="#14cf93"></path>
+                        fill="#2563eb"></path>
                     </svg>
                   </span>
                   <h6 class="inline fw-400">
@@ -62,12 +57,12 @@
         </div>
         <div class="col-lg-6 offset-lg-1 valign">
           <div class="full-width">
-            <div class="item sub-bg radius-10 d-flex align-items-center mb-30">
+            <div v-reveal class="item sub-bg radius-10 d-flex align-items-center mb-30">
               <div class="type">
                 <div class="icon-img-60 mb-10">
                   <img src="/light/assets/imgs/icon-img/shape1.png" alt="" />
                 </div>
-                <h5 class="fw-600">`Agentic AI Pilot Sprint`</h5>
+                <h5 class="fw-600">Agentic AI Pilot Sprint</h5>
               </div>
               <div class="cont ml-50">
                 <ul class="dot-list rest">
@@ -80,13 +75,13 @@
               <div class="mount ml-auto text-center">
                 <h3 class="numb-font"><span class="fz-20">from</span> SGD 7K</h3>
                 <div>
-                  <a href="/contact" class="butn butn-md butn-bord radius-30">
+                  <NuxtLink to="/contact" class="butn butn-md butn-bord radius-30">
                     <span class="fz-13">Start Here</span>
-                  </a>
+                  </NuxtLink>
                 </div>
               </div>
             </div>
-            <div class="item main-colorbg radius-10 text-dark d-flex align-items-center">
+            <div v-reveal="{ delay: 120 }" class="item main-colorbg radius-10 text-dark d-flex align-items-center">
               <div class="type">
                 <div class="icon-img-60 mb-10">
                   <img src="/light/assets/imgs/icon-img/shape03.png" alt="" />
@@ -104,9 +99,9 @@
               <div class="mount ml-auto text-center">
                 <h3 class="numb-font"><span class="fz-20">from</span> SGD 35K</h3>
                 <div>
-                  <a href="/contact" class="butn butn-md butn-bord text-dark radius-30">
+                  <NuxtLink to="/contact" class="butn butn-md butn-bord text-dark radius-30">
                     <span class="fz-13">Talk Scope</span>
-                  </a>
+                  </NuxtLink>
                 </div>
               </div>
             </div>
